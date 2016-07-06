4e879807f8363539fb5d4f039997dc25109d6f32
Jade Carter <jadebcarter@Jades-MBP-3.hsd1.ga.comcast.net>
2016-07-06 15:25:11 -0400

    initial

---
+++ js/app.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    // Initialize Swiper
+    var swiper = new Swiper('.swiper-container', {
+        pagination: '.swiper-pagination',
+        paginationClickable: true
+    });
+});
